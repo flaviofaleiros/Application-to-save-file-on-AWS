b5c19b0cb260fc61cef15ffe3a8a430703e65aa1
sil_ffaleiros <sil_ffaleiros@uolinc.com>
2020-04-21 17:35:55 -0300

    Ajustes no upload dos arquivos

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,17 @@ const multer = require('multer');
 const multerConfig = require('./config/multer')
 const Post = require('./models/Post');
 
+routes.get("/posts", async (req, res) => {
+    const posts = await Post.find();
 
+    return res.json(posts);
+});
 
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+    console.log(req);
 
-routes.post('/posts', multer(multerConfig).single("file"), async (req, res) => {
-    const { originalname: name, size, filename: key } = req.file;
+    const { originalname: name, size, filename: key, location: url = "" } = req.file;
+
     const post = await Post.create({
         name,
         size,
